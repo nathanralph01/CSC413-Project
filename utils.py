e2c2b6ffa4822d23a0b8af904c265988fff1354c
Nathan <nathan.ralph122@gmail.com>
2024-11-24 13:22:47 -0500

    Added functions to load and split data

--- utils.py
+++ utils.py
@@ -11,16 +11,17 @@ def load_data(path):
         raise Exception("File path {} does not exist".format(path))
     data_set = []
     with open(path, "r") as file:
-        story = []
+        story = ""
         for line in file:
             stripped = line.strip()
 
             if stripped == '':
                 if story:
                     data_set.append(story)
-                    story = []
+                    story = ""
             else:
-                story.append(stripped)
+                story +=  " " + stripped
+        data_set.append(story)
     return data_set
 
 
@@ -30,10 +31,13 @@ def split_data(data_set, train_split, val_split, test_split):
     
     Parameters:
         data_set: Data in list format
-        train_split: Percentage to split into training
+        train_split: Percentage to split into training (0.7 for 70%)
         val_split: Percentage to split into validation
         test_split: Percentage to split into testing
     """
-    pass
+    total = len(data_set)
+    train_num = int(total * train_split)
+    val_num = train_num + int(total*val_split)
 
-path = ''
+    return data_set[:train_num], data_set[train_num:val_num], data_set[val_num:]
+
