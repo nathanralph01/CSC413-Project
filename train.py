97f83236b3f9b3fc14b3d6f5bf664fde08b66ca1
nathanralph01 <nathan.ralph122@gmail.com>
2024-12-09 14:10:57 -0500

    Cleaned up training code

--- train.py
+++ train.py
@@ -1,105 +1,14 @@
 import torch
 import torch.optim as optim
 import matplotlib.pyplot as plt
-from model import BidirectionalRNNGenerator,StoryDataset
+from model import BidirectionalRNNGenerator
+from wrap import WrapDataset
 from utils import *
-import time
 import torch.nn.functional as F
 
 output_size = glove.vectors.shape[0]
 seq_length = 5
 
-# def acc(model, dataset, batch_size):
-#     r = 0
-#     c = 0
-#     loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=2)
-#     for i, (in_str, target) in enumerate(loader):
-#         current_batch_size = in_str.size(0)
-#         in_str, target = in_str.to(device), target.to(device)
-#         hidden = model.init_hidden(current_batch_size).to(device)
-#         output, hidden = model(in_str, hidden)
-
-#         # Reshape output to match target's dimensions
-#         output = output.reshape(batch_size, seq_length, -1)
-#         output_last = output[:, -1, :]
-
-#         # Apply softmax to the last dimension and get the top-k indices
-#         probabilities = F.softmax(output_last, dim=-1).detach().cpu()
-#         top_prob, top_idx = torch.topk(probabilities, k=1, dim=-1)
-#         next_word = top_idx.squeeze(-1)
-
-#         # Calculate accuracy
-#         r += float(torch.sum(next_word == target.cpu()))
-#         c += target.numel()
-#     return r / c
-
-
-
-
-# def train(model, train_data, val_data, learning_rate=0.001, batch_size=100, num_epochs=10):
-#     train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
-#     criterion = nn.CrossEntropyLoss().to(device)
-#     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-#     #print(f"Total number of batches: {len(train_loader)}", enumerate(train_loader).shape)
-
-#     seq_length = 5
-#     # these lists will be used to track the training progress
-#     # and to plot the training curve
-#     #iters, train_loss, train_acc, val_acc = [], [], [], []
-#     tloss = []
-#     tacc = []
-#     vacc = []
-#     count = 0 # count the number of iterations that has passed
-#     iters = []
-#     scaler = torch.cuda.amp.GradScaler()
-#     for e in range(num_epochs):
-#         torch.cuda.empty_cache()
-#         for i, (in_str, story) in enumerate(train_loader):
-#             current_batch_size = in_str.size(0)
-#             in_str, story = in_str.to(device), story.to(device)
-#             hidden = model.init_hidden(current_batch_size).to(device)
-#             optimizer.zero_grad()
-#             output, hidden = model(in_str, hidden)
-
-#             #reshape output
-#             output = output.reshape(batch_size, seq_length, -1)
-#             output_last = output[:, -1, :]
-#             output_flat = output_last.reshape(-1, output_size)
-#             targets_flat = story.reshape(-1)
-
-#             loss = criterion(output_flat, targets_flat) # TODO # (10, 300, embedding_size)
-
-#             scaler.scale(loss).backward() # propagate the gradients
-#             scaler.step(optimizer) # update the parameters
-#             scaler.update()
-#             optimizer.zero_grad()
-#             count += 1
-
-#             if count % 100 == 0:
-#                     iters.append(count)
-#                     t = acc(model, train_data, batch_size)
-#                     v = acc(model, val_data, batch_size)
-#                     tloss.append(float(loss))
-#                     tacc.append(t)
-#                     vacc.append(v)
-#                     print(count, "Loss:", float(loss), "Training Accuracy:", t, "Validation Accuracy:", v)
-
-#     #
-#     plt.figure()
-#     plt.plot(iters[:len(tloss)], tloss)
-#     plt.title("Loss over iterations")
-#     plt.xlabel("Iterations")
-#     plt.ylabel("Loss")
-#     plt.savefig("loss.png")
-
-#     plt.figure()
-#     plt.plot(iters[:len(tacc)], tacc)
-#     plt.plot(iters[:len(vacc)], vacc)
-#     plt.title("Accuracy over iterations")
-#     plt.xlabel("Iterations")
-#     plt.ylabel("Accuracy")
-#     plt.legend(["Train", "Validation"])
-#     plt.savefig("acc.png")
 
 def acc(model, dataset, batch_size):
     r = 0
@@ -114,11 +23,6 @@ def acc(model, dataset, batch_size):
         # Reshape output to match target's dimensions
         output = output.reshape(batch_size, seq_length, -1)
         output_last = output[:, -1, :]
-
-        # Apply softmax to the last dimension and get the top-k indices
-        # probabilities = F.softmax(output_last, dim=-1).detach().cpu()
-        # top_prob, top_idx = torch.topk(probabilities, k=1, dim=-1)
-        # next_word = top_idx.squeeze(-1)
 
         probabilities = torch.softmax(output_last, dim=-1).detach().cpu()
         next_word = torch.argmax(probabilities, dim=-1)
@@ -137,12 +41,8 @@ def train(model, train_data, val_data, learning_rate=0.001, batch_size=100, num_
     seq_length = 5
     # these lists will be used to track the training progress
     # and to plot the training curve
-    #iters, train_loss, train_acc, val_acc = [], [], [], []
-    tloss = []
-    tacc = []
-    vacc = []
+    iters, tloss, tacc, vacc = [], [], [], []
     count = 0 # count the number of iterations that has passed
-    iters = []
     scaler = torch.cuda.amp.GradScaler()
     for e in range(num_epochs):
         torch.cuda.empty_cache()
@@ -195,40 +95,26 @@ def train(model, train_data, val_data, learning_rate=0.001, batch_size=100, num_
 
 
 
-##TODO CREATE CUSTOM BATCH FUNCTION
-
+# Run to train the model. The state dict will be saves as model_state.pth
 if __name__ == '__main__':
-    #start = time.time()
     os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
     data = load_data('data/stories2.txt') # using samller txt file for testing purposes
 
     # convert data to sequences in order to predict next character
     seq_data = create_training_sequences(data, 5)
     # embed data
-    seq_data_embed = embed_data_tuples(seq_data)
-    #print(seq_data_embed[0])
-
+    seq_data_embed = embed_data_stories(seq_data)
     # split to train, val, test sets ->> MAY NEED TO CHANGE VAL AND TEST SETS
     train_data, val_data, test_data = split_data(seq_data_embed, 0.7, 0.15)
+    # Wrap the data so that it is compatible with DataLoader
+    wrapped_data = WrapDataset(train_data[:1])
+    wrapped_data_val = WrapDataset(val_data[:1])
+    
     model = BidirectionalRNNGenerator().to(device)
-    wrapped_data = StoryDataset(train_data[:1])
-    wrapped_data_val = StoryDataset(val_data[:1])
-    #train_loader = torch.utils.data.DataLoader(wrapped_data, batch_size=2, shuffle=True)
+
     print("Beginning training...")
-    train(model, wrapped_data, wrapped_data_val, batch_size=2, num_epochs=6, learning_rate=0.003)
-    x,t = wrapped_data[0]
-    print("FROM TRAINING DATA: ")
-    print(test_word_rep(x), glove.itos[t])
-    x,t = x.to(device), t.to(device)
-    hidden = model.init_hidden(1).to(device)
-    hidden = hidden.squeeze(1)
-    output = model(x, hidden)
-
-    # Note: previous hidden states are used when generating sequences (during test time). Not when training
-
-    probabilities = F.softmax(output[-1], dim=0).detach().cpu()
-    top_prob, top_idx = torch.topk(probabilities, k=1)
-    next_word = top_idx.numpy()[0]
-    print(test_word_rep(next_word))
+    train(model, wrapped_data, wrapped_data_val, batch_size=2, num_epochs=2, learning_rate=0.003)
+    print("Training complete")
+    torch.save(model.state_dict(), "model_state.pth")
 
 
